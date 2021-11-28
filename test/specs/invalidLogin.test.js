8c6636887e1fc6acf187c1dea92404c5de749113
BryanMerlo5 <bryan.merlosd@gmail.com>
2021-11-28 12:53:07 -0600

    adding invalidLogin.test.js

---
+++ test/specs/invalidLogin.test.js
@@ -0,0 +1,20 @@
+const LoginPage = require('../../src/pages/login.page');
+const HomePage = require('../../src/pages/home.page');
+const data = require('../../src/data/enum.json');
+const Utils = require('../../src/utils/utils');
+const expect = require('chai').expect;
+
+describe('My Login application', () => {
+    it('Should be displayed an error message with invalid credentials', async () => {
+        let timeOut = 10000;
+        let products = 'PRODUCTS';
+        await LoginPage.open();
+        await Utils.waitForEnabled(LoginPage.imgLogo, true, timeOut);
+        await LoginPage.login(data.user.invalid, data.pass);
+        // Validate error message is displayed
+        const isErrorDisplayed = await Utils.isDisplayed('[data-test="error"]');
+        expect(isErrorDisplayed).to.be.equal(true);
+    });
+});
+
+
