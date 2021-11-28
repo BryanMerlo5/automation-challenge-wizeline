52f5230957b9f4a55fd10bb7d5bfb88bf7de8b97
BryanMerlo5 <bryan.merlosd@gmail.com>
2021-11-27 23:00:57 -0600

    adding utils

---
+++ src/utils/utils.js
@@ -0,0 +1,42 @@
+class Utils {
+    /**
+    * Opens a sub page of the page
+    * @param url url of the website (e.g. www.google.com/)
+    * @param path path of the sub page (e.g. /path/to/page.html)
+    */
+    async openWebPage (url, path) {
+        return browser.url(`${url}${path}`);
+    }
+
+    /**
+    * Waits until the element is displayed
+    * @param selector selector, JS Function, or Matcher object to fetch a certain element
+    * @param falseCase if true it waits for the opposite (default: false)
+    * @param timeOut time in ms
+    */
+    async waitForEnabled (selector, falseCase, timeOut) {
+        const ms = timeOut;
+        // selector could be a string or a getter
+        if (typeof selector === 'string' || selector instanceof String)
+            return await $(selector).waitForDisplayed(ms, !!falseCase);
+        else
+            return await selector.waitForDisplayed(ms, !!falseCase);
+    };
+
+    /**
+    * Waits until the element is displayed
+    * @param selector selector, JS Function, or Matcher object to fetch a certain element
+    * @param falseCase if true it waits for the opposite (default: false)
+    * @param timeOut time in ms
+    */
+     async isDisplayed (selector) {
+        // selector could be a string or a getter
+        if (typeof selector === 'string' || selector instanceof String){
+            return await $(selector).isDisplayed();
+        } else {
+            return await selector.isDisplayed();
+        }
+    };
+}
+
+module.exports = new Utils();
